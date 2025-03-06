1dd81c9fff4d935f5c4745b754d241c016fbfe7c
Chase Taylor <chase@Chases-Space-MacBook-Pro.local>
2025-03-06 13:17:06 -0700

    Step 3: Created a story structure and an array with more story parts

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,16 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    let stories = [
+        ["You see a fork in the road.", "Take a left.", "Take a right."],
+        ["You see a tiger.", "Shout for help.", "Play dead."],
+        ["You find a treasure chest.", "Open it.", "Check for traps."],
+    ]
+    
+    let journey = Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right.")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        storyLabel.text = journey.title
+        choice1Button.setTitle(journey.choice1, for: .normal)
+        choice2Button.setTitle(journey.choice2, for: .normal)
 
     }
 
